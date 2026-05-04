bb7a5e02c62c65855009a5302aece48634f6ef86
akmalropip <akmalropip@gmail.com>
2026-05-04 15:54:27 +0700

    pembuatan object dan pemberian nilai

--- staticFunction.cpp
+++ staticFunction.cpp
@@ -25,3 +25,23 @@ public:
         setID();
     }
 };
+
+int Mahasiswa::nim = 0;
+
+//pendefinisial prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    //pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    
+
